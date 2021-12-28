ef346d54cdaa5047ab0bbcd5612a879f23d4c14a
suleiman01x <suleiman01x@users.noreply.github.com>
2021-12-29 05:51:43 +0900

    bug fix

--- houou/naga.py
+++ houou/naga.py
@@ -11,16 +11,16 @@ class Naga:
 		pred_str = self.response.text
 
 		pred_str1 = pred_str.replace('"actor":', '\n"actor":')
-		pred_str2 = pred_str1.replace('"real_dahai":', '\n"read_dahai":')
 
 		pred_pattern1 = re.compile(r'"real_dahai": "(.*?)", "pred_dahai": "(.*?)"')
-		pred_str3 = pred_pattern1.sub(r'\n"real_dahai": "\1", "pred_dahai": "\2"\n', pred_str2)
+		pred_str3 = pred_pattern1.sub(r'\n"real_dahai": "\1", "pred_dahai": "\2"\n', pred_str1)
+
 
 		pred_pattern2 = re.compile(r'^.*DOCTYPE html.*\n')
 		pred_str4 = pred_pattern2.sub(r'', pred_str3)
 
 		pred_str4 = pred_str4.replace('"', '')
-		return pred_str4
+		return "hello"
 
 if __name__ == "__main__":
 	testurl = 'https://naga.dmv.nico/htmls/2a42a9763cc316d8b39a9a3f37d6afaaa233b3ac050bb44099cc68913d31284fv2_0_0.html?tw=3'
